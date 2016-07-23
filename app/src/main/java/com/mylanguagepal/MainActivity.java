761dbb78efe07cbcd31a5084e7fb85f430e822dc
vphoenix1972 <vphoenix1972@gmail.com>
2016-07-23 10:59:56 +0300

    Revert "Test commit"
    This reverts commit 13d4bf835659d6cc3bc279869fd964528246a329.

--- app/src/main/java/com/mylanguagepal/MainActivity.java
+++ app/src/main/java/com/mylanguagepal/MainActivity.java
@@ -46,7 +46,6 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        //
         // Show default fragment
         showFragment(navigationView.getMenu().findItem(R.id.menu_item_tags));
     }
